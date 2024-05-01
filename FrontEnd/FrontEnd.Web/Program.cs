680053adf5bae89866b7f1a8d516f8706421d91d
BenasB <29711974+BenasB@users.noreply.github.com>
2024-05-01 12:31:15 +0300

    Init FrontEnd

---
+++ FrontEnd/FrontEnd.Web/Program.cs
@@ -0,0 +1,24 @@
+using FrontEnd.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
