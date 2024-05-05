580d4bfacd849d2c1e0e14aebe7b86acd040026c
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2024-05-05 14:08:49 +0300

    Use Fluent UI component library (#12)
    * Get started with Fluent UI
    
    * Initialize layout
    
    * Make all pages use Fluent UI
    
    * More merging

--- FrontEnd/FrontEnd.Web/Program.cs
+++ FrontEnd/FrontEnd.Web/Program.cs
@@ -1,10 +1,13 @@
 using FrontEnd.Web.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 using FrontEnd.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddHttpClient<IRecipeService, RecipeService>();
 builder.Services.AddHttpClient<IIdentityService, IdentityService>();
