19e4f9a04f5b60bc28ec366f411c09fc8226650a
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-05-17 22:54:23 +0300

    - moved DTOs from Application to Public layer

--- Recipes/Recipes.Public/DTOs/Recipes/RecipeUpdateDTO.cs
+++ Recipes/Recipes.Public/DTOs/Recipes/RecipeUpdateDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Recipes.Application.DTOs.Recipes;
+﻿namespace Recipes.Public.DTOs.Recipes;
 
 public class RecipeUpdateDTO
 {
