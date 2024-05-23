9e0e3b612cefcc613528f3b04eba60a828d9750c
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-05-23 18:47:01 +0300

    Forgot comma

--- Recipes/Recipes.API/Controllers/RecipesController.cs
+++ Recipes/Recipes.API/Controllers/RecipesController.cs
@@ -129,8 +129,8 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
             UpdatedAt = recipeEntity.UpdatedAt,
             Ingredients = recipeEntity.Ingredients,
             Instructions = recipeEntity.Instructions,
-            Image = recipeEntity.Image
-            Tags = recipeEntity.Tags.Select(t => t.Tag).Select(TagsController.GetTagFromEntity),
+            Image = recipeEntity.Image,
+            Tags = recipeEntity.Tags.Select(t => t.Tag).Select(TagsController.GetTagFromEntity)
         };
     }
 }
