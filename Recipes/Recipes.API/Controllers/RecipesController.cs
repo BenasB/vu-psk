2a31b220c199bc75b96487fc34a5bbfa873f2613
Adomas Vensas <90962230+adomas-vensas@users.noreply.github.com>
2024-05-24 19:48:18 +0300

    Create and Update endpoints for Recipes (#40)
    * - Added Create endpoint for recipes
    
    * Added UPDATE endpoint for recipes
    
    * Removed unused imports
    
    * Renamed RecipeDTO to RecipeCreateUpdateDTO
    
    * added a space
    
    * - Added return object for Create endpoint
    - Added a new status code
    
    * Fixed naming inconsistencies
    
    * - Refactored
    
    * Rytis and Sarunas comment fixes
    
    * Removed empty line.cs
    
    Co-authored-by: Šarūnas Griškus <86420873+6ar8nas@users.noreply.github.com>
    
    * Added space
    
    Co-authored-by: Rytis-Sapka <91343849+Rytis-Sapka@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: adomas-vensas <adoma@DESKTOP-09F4KST>
    Co-authored-by: Šarūnas Griškus <86420873+6ar8nas@users.noreply.github.com>
    Co-authored-by: Rytis-Sapka <91343849+Rytis-Sapka@users.noreply.github.com>

--- Recipes/Recipes.API/Controllers/RecipesController.cs
+++ Recipes/Recipes.API/Controllers/RecipesController.cs
@@ -8,8 +8,22 @@ namespace Recipes.API.Controllers;
 
 [ApiController]
 [Route("recipes")]
-public class RecipesController(IGenericRepository<RecipeEntity> recipesRepository, IGenericRepository<TagRecipeEntity> tagRecipesRepository) : ControllerBase
+public class RecipesController : ControllerBase
 {
+    private readonly IGenericRepository<RecipeEntity> _recipesRepository;
+    private readonly IGenericRepository<TagRecipeEntity> _tagRecipeRepository;
+    private readonly IGenericRepository<TagEntity> _tagRepository;
+    
+    public RecipesController(
+        IGenericRepository<RecipeEntity> recipeRepository,
+        IGenericRepository<TagRecipeEntity> tagRecipeRepository,
+        IGenericRepository<TagEntity> tagRepository)
+    {
+        _tagRecipeRepository = tagRecipeRepository;
+        _recipesRepository = recipeRepository;
+        _tagRepository = tagRepository;
+    }
+
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -18,7 +32,7 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
         IEnumerable<RecipeEntity> recipes;
         try
         {
-            recipes = await recipesRepository.GetAllAsync();
+            recipes = await _recipesRepository.GetAllAsync();
         }
         catch (Exception)
         {
@@ -39,7 +53,7 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
         RecipeEntity? recipe;
         try
         {
-            recipe = recipesRepository.GetById(recipeId);
+            recipe = _recipesRepository.GetById(recipeId);
         }
         catch (Exception)
         {
@@ -62,7 +76,7 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
         RecipeEntity? recipe;
         try
         {
-            recipe = recipesRepository.GetById(recipeId);
+            recipe = _recipesRepository.GetById(recipeId);
         }
         catch (Exception)
         {
@@ -73,8 +87,8 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
 
         try
         {
-            recipesRepository.Delete(recipe);
-            await recipesRepository.SaveChangesAsync();
+            _recipesRepository.Delete(recipe);
+            await _recipesRepository.SaveChangesAsync();
         }
         catch (Exception)
         {
@@ -82,6 +96,65 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
         }
 
         return NoContent();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> CreateRecipe(RecipeCreateUpdateDTO request)
+    {
+        try
+        {
+            await InsertNewTags(request.Tags);
+
+            var newRecipe = GetRecipeFromDTO(request);
+            newRecipe.Tags = await GetEntityTags(newRecipe, request.Tags);
+
+            _recipesRepository.Insert(newRecipe);
+            await _recipesRepository.SaveChangesAsync();
+
+            Recipe response = GetRecipeFromEntity(newRecipe);
+
+            return Created($"/recipes/{response.Id}", response);
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpPut("{recipeId:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> UpdateRecipe(int recipeId, [FromBody] RecipeCreateUpdateDTO request)
+    {
+        RecipeEntity? recipeToUpdate;
+        
+        try
+        {
+            recipeToUpdate = _recipesRepository.GetById(recipeId);
+
+            if (recipeToUpdate == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+
+            await InsertNewTags(request.Tags);
+
+            recipeToUpdate = UpdateOldRecipeEntity(recipeToUpdate, request);
+            recipeToUpdate.Tags = await GetEntityTags(recipeToUpdate, request.Tags);
+
+            _recipesRepository.Update(recipeToUpdate);
+            await _recipesRepository.SaveChangesAsync();
+
+            return NoContent();
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
     }
 
     [HttpDelete("{recipeId:int}/tags/{tagId:int}")]
@@ -93,7 +166,7 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
         TagRecipeEntity? recipeTag;
         try
         {
-            recipeTag = tagRecipesRepository.GetById(recipeId, tagId);
+            recipeTag = _tagRecipeRepository.GetById(recipeId, tagId);
         }
         catch (Exception)
         {
@@ -104,8 +177,8 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
 
         try
         {
-            tagRecipesRepository.Delete(recipeTag);
-            await tagRecipesRepository.SaveChangesAsync();
+            _tagRecipeRepository.Delete(recipeTag);
+            await _tagRecipeRepository.SaveChangesAsync();
         }
         catch (Exception)
         {
@@ -131,5 +204,70 @@ public class RecipesController(IGenericRepository<RecipeEntity> recipesRepositor
             Image = recipeEntity.Image,
             Tags = recipeEntity.Tags.Select(t => t.Tag).Select(TagsController.GetTagFromEntity)
         };
+    }
+
+    private RecipeEntity UpdateOldRecipeEntity(RecipeEntity oldRecipeEntity, RecipeCreateUpdateDTO recipeDTO)
+    {
+        oldRecipeEntity.Title = recipeDTO.Title;
+        oldRecipeEntity.AuthorId = recipeDTO.AuthorId;
+        oldRecipeEntity.Description = recipeDTO.Description;
+        oldRecipeEntity.CookingTime = recipeDTO.CookingTime;
+        oldRecipeEntity.Servings = recipeDTO.Servings;
+        oldRecipeEntity.Ingredients = recipeDTO.Ingredients;
+        oldRecipeEntity.Instructions = recipeDTO.Instructions;
+        oldRecipeEntity.Image = recipeDTO.Image;
+
+        return oldRecipeEntity;
+    }
+
+    private RecipeEntity GetRecipeFromDTO(RecipeCreateUpdateDTO recipeDTO)
+    {
+        return new RecipeEntity()
+        {
+            Title = recipeDTO.Title,
+            AuthorId = recipeDTO.AuthorId,
+            Description = recipeDTO.Description,
+            CookingTime = recipeDTO.CookingTime,
+            Servings = recipeDTO.Servings,
+            Ingredients = recipeDTO.Ingredients,
+            Instructions = recipeDTO.Instructions,
+            Image = recipeDTO.Image
+        };
+    }
+
+    private async Task InsertNewTags(IList<string> allTagNames)
+    {
+        if (allTagNames == null || allTagNames.Count == 0) return;
+
+        var allTags = await _tagRepository.GetAllAsync();
+        var existingTags = new List<TagEntity>();
+
+        foreach (var tag in allTags)
+        {
+            if (allTagNames.Contains(tag.Name))
+            {
+                existingTags.Add(tag);
+            }
+        }
+
+        List<string> newTags = allTagNames.Except(existingTags.Select(x => x.Name)).ToList();
+
+        foreach (var tagName in newTags)
+        {
+            _tagRepository.Insert(new TagEntity { Name = tagName });
+        }
+
+        await _recipesRepository.SaveChangesAsync();
+    }
+
+    private async Task<List<TagRecipeEntity>> GetEntityTags(RecipeEntity recipeEntity, IList<string> allTagNames)
+    {
+        var allTags = await _tagRepository.GetAllAsync();
+
+        return allTags
+                .Where(x => allTagNames.Contains(x.Name))
+                .ToHashSet()
+                .Select(x => new TagRecipeEntity { Tag = x, Recipe = recipeEntity })
+                .ToList();
     }
 }
